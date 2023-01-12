304f5de7fad5399aebdcd06396929f06f579f053
mmorenoregalado <mmorenoregalado@gmail.com>
2023-01-12 00:15:48 -0600

    feat(config): add file for config

---
+++ src/action_config.ts
@@ -0,0 +1,11 @@
+export interface ActionConfig {
+  github_token: string;
+  repo: string;
+  owner: string;
+}
+
+export const config: ActionConfig = {
+  github_token: process.env.GITHUB_TOKEN as string,
+  repo: process.env.REPOSITORY_NAME as string,
+  owner: process.env.REPOSITORY_OWNER as string
+}
