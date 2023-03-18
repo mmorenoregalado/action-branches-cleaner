b4ea6a98ac8d3c3c3f4b20750e4eba61c0883559
mmorenoregalado <mmorenoregalado@gmail.com>
2023-03-18 14:26:35 -0600

    feat(clean-branches): change github action from typescript to brash script

--- __tests__/main.test.ts
+++
@@ -1,5 +0,0 @@
-import {expect, test} from '@jest/globals'
-
-test('dummy test', async () => {
-  await expect(true).toBe(true)
-})
--- src/domain/GitHubRepositoryRepository.ts
+++
@@ -1,7 +0,0 @@
-import {Branch} from './GitHubRepository'
-
-export interface GitHubRepositoryRepository {
-  branches(): Promise<Branch[]>
-  mergedBranches(branches: Branch[]): Promise<string[]>
-  deleteBranches(branches: string[]): Promise<void>
-}
