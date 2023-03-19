a19aa8290fc7db00c3e8e6326a9df85fce915f52
Miguel Moreno <51204523+mmorenoregalado@users.noreply.github.com>
2023-03-18 23:09:08 -0600

    feat/clean-commands (#46)
    * chore(clean-branches): cleanup commands
    
    * fix(clean-branches): delete remote branches
    
    * fix(clean-branches): only delte branches no files
    
    * fix(clean-branches): debuguin branches
    
    * fix(clean-branches): omite base branches from the result merged
    
    * fix(clean-branches): skip empty results
    
    * fix(clean-branches): filter branches not merged
    
    * fix(clean-branches): remote branches
    
    * fix(clean-branches): remote branches debuguin
    
    * fix(clean-branches): change to github api
    
    * fix(clean-branches): api response
    
    * fix(clean-branches): change edpoint

--- src/main.sh
+++ src/main.sh
@@ -2,57 +2,21 @@
 
 main() {
   BASE_BRANCHES=$1
-  DAYS_OLD_THRESHOLD=$2
   GITHUB_TOKEN=$3
 
-  git config --global user.email "action@github.com"
-  git config --global user.name "GitHub Action"
+  # Get merged PRs
+  merged_prs=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
+    "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls?state=closed&base=$BASE_BRANCHES" |
+    jq -r '.[] | select(.merged_at != null) | .head.ref')
 
-  REPO_URL=$(git config --get remote.origin.url)
-  AUTHENTICATED_REPO_URL="https://${GITHUB_TOKEN}@${REPO_URL#*//}"
-  git remote set-url origin "$AUTHENTICATED_REPO_URL"
-
-  git fetch --all --prune
-
-  # Leer ramas base y convertirlas en array
-  IFS=',' read -ra BRANCHES <<<"$BASE_BRANCHES"
-
-  for base_branch in "${BRANCHES[@]}"; do
-    git checkout "$base_branch"
-
-    # Eliminar ramas fusionadas
-    merged_branches=$(git branch --merged)
-    branches_to_delete=""
-
-    for branch in $merged_branches; do
-      branch=$(echo "$branch" | xargs) # Remove leading/trailing whitespaces
-      skip_branch=false
-      for protected_branch in "${BRANCHES[@]}"; do
-        if [[ $branch == *"$protected_branch"* ]]; then
-          skip_branch=true
-          break
-        fi
-      done
-      if [[ $skip_branch == false ]] && [[ $branch != *"*"* ]]; then
-        branches_to_delete+="$branch "
-      fi
-    done
-
-    if [ -n "$branches_to_delete" ]; then
-      echo "Branches to delete: $branches_to_delete"
-    else
-      echo "No branches to delete."
+  # Loop through branches and delete them
+  for branch in $merged_prs; do
+    if [ "$branch" != "$BASE_BRANCHES" ]; then
+      echo "Deleting merged branch: $branch"
+      # Delete merged branch
+      curl -X DELETE -H "Authorization: token $GITHUB_TOKEN" \
+        "https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs/heads/$branch"
     fi
-
-    # Eliminar ramas inactivas según el umbral definido
-    for branch in $(git for-each-ref --format='%(refname:short)' refs/heads); do
-      last_commit_date=$(git show -s --format="%ct" "$branch")
-      current_date=$(date +%s)
-      days_since_last_commit=$(((current_date - last_commit_date) / 86400))
-
-      if [ $days_since_last_commit -ge "$DAYS_OLD_THRESHOLD" ]; then
-        git push origin --delete "$branch"
-      fi
-    done
   done
+
 }
