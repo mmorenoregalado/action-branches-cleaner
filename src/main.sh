47868f8bb5a40b8025719e884792e5c83ca7fae0
mmorenoregalado <mmorenoregalado@gmail.com>
2023-03-22 21:25:37 -0600

    feat(clean-older): remove automatically inactive branches

--- src/main.sh
+++ src/main.sh
@@ -1,12 +1,11 @@
 #!/bin/env bash
 
-# Importar funciones de github.sh
 source "$BRANCHES_CLEANER_HOME"/src/github.sh
-# Importar funciones de cleanup.sh
 source "$BRANCHES_CLEANER_HOME"/src/cleanup.sh
 
 main() {
   BASE_BRANCHES_STR=$1
+  DAYS_OLD_THRESHOLD=$2
   GITHUB_TOKEN=$3
 
   GITHUB_API_URL="https://api.github.com/repos/$GITHUB_REPOSITORY"
@@ -16,15 +15,21 @@ main() {
 
   IFS=',' read -ra BASE_BRANCHES <<<"$BASE_BRANCHES_STR"
 
-  export BASE_BRANCHES
-
   closed_prs=$(github::get_closed_prs)
 
   merged_prs=$(github::get_merged_prs)
 
   not_merged_prs=$(comm -23 <(echo "$closed_prs" | sort) <(echo "$merged_prs" | sort))
 
-  cleanup::delete_merged_branches "$merged_prs"
+  for base_branch in "${BASE_BRANCHES[@]}"; do
 
-  cleanup::delete_unmerged_branches "$not_merged_prs"
+    # Delete merged branches
+    cleanup::delete_merged_branches "$merged_prs" "$base_branch"
+
+    # Delete unmerged branches
+    cleanup::delete_unmerged_branches "$not_merged_prs" "$base_branch"
+
+    #Delete inactive branches
+    cleanup::delete_inactive_branches "$DAYS_OLD_THRESHOLD"
+  done
 }
