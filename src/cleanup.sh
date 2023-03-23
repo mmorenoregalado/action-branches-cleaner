538b64b8a10be85e06bc2a7a52f802ecdf095eb8
mmorenoregalado <mmorenoregalado@gmail.com>
2023-03-23 00:01:37 -0600

    fix(base_branches): only check one time base branches

--- src/cleanup.sh
+++ src/cleanup.sh
@@ -2,22 +2,16 @@
 
 cleanup::delete_merged_branches() {
   local merged_prs=$1
-  local base_branch=$2
   for branch in $merged_prs; do
-    if [ "$branch" != "$base_branch" ]; then
-      echo "Deleting merged branch: $branch"
-      github::delete_branch "$branch"
-    fi
+    echo "Deleting merged branch: $branch"
+    github::delete_branch "$branch"
   done
 }
 
 cleanup::delete_unmerged_branches() {
   local not_merged_prs=$1
-  local base_branch=$2
   for branch in $not_merged_prs; do
-    if [ "$branch" != "$base_branch" ]; then
-      echo "Deleting not merged branch: $branch"
-      github::delete_branch "$branch"
-    fi
+    echo "Deleting not merged branch: $branch"
+    github::delete_branch "$branch"
   done
 }
