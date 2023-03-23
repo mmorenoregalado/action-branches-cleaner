bc63404df01282a56fb6908e3a1bec12490577cf
Miguel Moreno <51204523+mmorenoregalado@users.noreply.github.com>
2023-03-23 00:05:29 -0600

    fix/base_branches (#56)
    * fix(base_branches): check only when a branch is to be deleted

--- src/main.sh
+++ src/main.sh
@@ -1,11 +1,12 @@
 #!/bin/env bash
 
+# Importar funciones de github.sh
 source "$BRANCHES_CLEANER_HOME"/src/github.sh
+# Importar funciones de cleanup.sh
 source "$BRANCHES_CLEANER_HOME"/src/cleanup.sh
 
 main() {
   BASE_BRANCHES_STR=$1
-  DAYS_OLD_THRESHOLD=$2
   GITHUB_TOKEN=$3
 
   GITHUB_API_URL="https://api.github.com/repos/$GITHUB_REPOSITORY"
@@ -15,19 +16,15 @@ main() {
 
   IFS=',' read -ra BASE_BRANCHES <<<"$BASE_BRANCHES_STR"
 
+  export BASE_BRANCHES
+
   closed_prs=$(github::get_closed_prs)
 
   merged_prs=$(github::get_merged_prs)
 
   not_merged_prs=$(comm -23 <(echo "$closed_prs" | sort) <(echo "$merged_prs" | sort))
 
-  for base_branch in "${BASE_BRANCHES[@]}"; do
+  cleanup::delete_merged_branches "$merged_prs"
 
-    cleanup::delete_merged_branches "$merged_prs" "$base_branch"
-
-    cleanup::delete_unmerged_branches "$not_merged_prs" "$base_branch"
-
-    #Delete inactive branches
-    cleanup::delete_inactive_branches "$DAYS_OLD_THRESHOLD"
-  done
+  cleanup::delete_unmerged_branches "$not_merged_prs"
 }
